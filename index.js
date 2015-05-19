0ae9dbd34b6d4cbf41a403444a0807ae8838fa5a
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-05-18 22:17:52 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').load();
+
+var connect = require("connect");
+var app = connect();
+var http = require("http");
+var UPS = require("shipping-ups");
+
+var ups = UPS({
+  environment: 'live',
+  username: process.env.UPS_USERNAME,
+  password: process.env.UPS_PASSWORD,
+  access_key: process.env.UPS_ACCESS_KEY,
+  imperial: true
+});
+
+app.use(require("compression")());
+app.use(function(request, response, next) {
+  var match = request.originalUrl.match(/\/(.*)+/);
+  if (!match) {
+    next();
+    return;
+  }
+
+  ups.track(match[1], function(err, results) {
+    var result = {};
+    if (err) {
+      result = { error: err };
+    } else {
+      result = results;
+    }
+
+    response.end(JSON.stringify(result));
+  });
+});
+
+app.listen(process.env.PORT || 3000);
