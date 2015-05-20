a3af0ec4882637a1dcc7d5512392071932d64721
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-05-20 07:24:17 -0700

    Added moment, delivery date if available

--- src/components/package-info.jsx
+++ src/components/package-info.jsx
@@ -1,6 +1,15 @@
 var React = require("react");
+var moment = require("moment");
 
 var PackageInfo = React.createClass({
+  deliveryDateAvailable: function() {
+    return !!this.props.shipment.ScheduledDeliveryDate;
+  },
+
+  deliveryDate: function() {
+    return moment(this.props.shipment.ScheduledDeliveryDate, "YYYYMMDD").format("ddd MMM Do, YYYY");
+  },
+
   description: function() {
     var activity = this.props.shipment.Package.Activity;
     var status;
@@ -14,7 +23,12 @@ var PackageInfo = React.createClass({
   },
 
   render: function() {
-    return <span>{this.description()}</span>;
+    return (
+      <div>
+        <span>{this.description()}</span><br/>
+        {this.deliveryDateAvailable() ? <span>Scheduled Delivery Date: {this.deliveryDate()}</span> : ''}
+      </div>
+    );
   }
 });
 
