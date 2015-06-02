953a0841e73f4ab275ec0890035cd7f15d3bee0a
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-06-02 16:44:17 -0700

    Updated server router to match only alphanumerics and return a status key

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ var app = connect();
 var TrackingStore = require('./src/tracking-store.js');
 
 app.use(require("compression")());
-app.use('/api', function(request, response, next) {
-  var match = request.originalUrl.match(/\/api\/(.+)/);
+app.use('/api', function(request, response/* , next */) {
+  var match = request.originalUrl.match(/\/api\/([A-Za-z0-9]+)/);
   if (!match) {
     response.end(JSON.stringify({ error: "No tracking number provided" }));
   }
 
   TrackingStore.get(match[1])
     .then(function(result) {
-      response.end(JSON.stringify(result));
+      response.end(JSON.stringify({ status: 'ok', result: result }));
     })
     .catch(function(error) {
-      response.end(JSON.stringify(error));
+      response.end(JSON.stringify({ status: 'error', error: error }));
     });
 });
 app.use(serveStatic("dist"));
-app.use(function(request, response, next) {
+app.use(function(request, response/* , next */) {
   var stream = send(request, "dist/index.html", {});
   stream.pipe(response);
 });
 
 app.listen(process.env.PORT || 3000);
+
