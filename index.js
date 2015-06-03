26d9558b2700155b7f1d1c40dbd56287eba4ba2a
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-06-02 20:53:53 -0700

    Using Express, extracted ApiHandler into module

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 require('dotenv').load();
 
-var connect = require("connect");
+var express = require("express");
+var app = express();
 var serveStatic = require("serve-static");
 var send = require("send");
-var app = connect();
-var TrackingStore = require('./src/tracking-store.js');
+var ApiHandler = require("./src/api-handler.js");
 
 app.use(require("compression")());
-app.use('/api', function(request, response/* , next */) {
-  var match = request.originalUrl.match(/\/api\/([A-Za-z0-9]+)/);
-  if (!match) {
-    response.end(JSON.stringify({ error: "No tracking number provided" }));
-  }
-
-  TrackingStore.get(match[1])
-    .then(function(result) {
-      response.end(JSON.stringify({ status: 'ok', result: result }));
-    })
-    .catch(function(error) {
-      response.end(JSON.stringify({ status: 'error', error: error }));
-    });
-});
+app.get(/\/api\/([A-Za-z0-9]+)/, ApiHandler);
 app.use(serveStatic("dist"));
 app.use(function(request, response/* , next */) {
   var stream = send(request, "dist/index.html", {});
---
+++ src/api-handler.js
@@ -0,0 +1,16 @@
+var TrackingStore = require('./src/tracking-store.js');
+
+module.exports = function(request, response/* , next */) {
+  var match = request.originalUrl.match(/\/api\/([A-Za-z0-9]+)/);
+  if (!match) {
+    response.end(JSON.stringify({ error: "No tracking number provided" }));
+  }
+
+  TrackingStore.get(match[1])
+    .then(function(result) {
+      response.end(JSON.stringify({ status: 'ok', result: result }));
+    })
+    .catch(function(error) {
+      response.end(JSON.stringify({ status: 'error', error: error }));
+    });
+};
