08efad27cd57a70513aca3026d106e38e5b5081e
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-06-02 17:00:08 -0700

    Removed console.log

--- src/components/usps-package-info.jsx
+++ src/components/usps-package-info.jsx
@@ -2,7 +2,6 @@ var React = require("react");
 
 var USPSPackageInfo = React.createClass({
   render: function() {
-    console.log("[USPSPackageInfo] this.props.shipment: ", this.props.shipment);
     return <div>{this.props.shipment.TrackResponse.TrackInfo.TrackSummary}</div>;
   }
 });
